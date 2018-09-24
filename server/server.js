bd1fe5f213e972de2cdd79ea8036b870eb345105
Divyanshu Rathore <divyanshu03@live.com>
2018-09-24 20:02:11 +0530

    Changes for heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let { ObjectID } = require('mongodb');
 
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
 let { User } = require('./models/user');
 
 let app = express();
+let port = process.env.PORT || 3000; 
 
 app.use(bodyParser.json());
 
@@ -27,8 +29,24 @@ app.get('/todos', (req, res) => {
   }, (e) => res.status(400).send(e));
 });
 
-app.listen(3000, () => {
-  console.log('Started on port: 3000');
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => res.status(400).send());
+});
+
+app.listen(port, () => {
+  console.log(`Started on port: ${port}`);
 });
 
 module.exports = { app };
